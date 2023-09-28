617523f5e36efc612c5d7150eb476478c27f8671
Thomas Mazzotta <thomasmazzotta99@gmail.com>
2023-09-28 17:23:44 +0200

    ok

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-fetch("https://api.pexels.com/v1/search?query=dog", {
-  method: "GET",
-  headers: {
-    Authorization: "82rmgDtE1aXqVG2filAdCKchb6B1CgZDThq8diwmTt4h6TnCnmFAmjTo",
-  },
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Error", error));
+const searchInput = document.getElementById("search-input");
+const searchForm = document.getElementById("search-form");
+const gallery = document.getElementById("gallery");
 
-const fetchImages = (query, id) => {
-  const section = document.querySelector();
+searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const query = searchInput.value;
+  fetchImages(query);
+});
+
+const fetchImages = (query) => {
+  fetch("https://api.pexels.com/v1/search?query=" + query, {
+    method: "GET",
+    headers: {
+      Authorization: "82rmgDtE1aXqVG2filAdCKchb6B1CgZDThq8diwmTt4h6TnCnmFAmjTo",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      gallery.innerHTML = "";
+      for (let i = 0; i < data.photos.slice(0, 20).length; i++) {
+        gallery.innerHTML +=
+          /*html*/
+          `<div class="col my-auto">
+          <img src="${data.photos[i].src.large}" class="img-fluid mb-2">
+        </div>`;
+      }
+    })
+    .catch((error) => console.error("Error", error));
 };
